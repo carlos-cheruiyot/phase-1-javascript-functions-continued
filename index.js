bf6e9348b89f13ab92f3a34806021d2687e1b46c
carlos <carloskip25@gmail.com>
2025-03-23 16:56:57 +0300

    code grade

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+// 1. Saturday Fun Function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // 2. Monday Work Function
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // 3. Wrap Adjective Function
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Examples of usage:
+  
+  // 1. Testing saturdayFun
+  console.log(saturdayFun()); // Output: "This Saturday, I want to roller-skate!"
+  console.log(saturdayFun("read a book")); // Output: "This Saturday, I want to read a book!"
+  
+  // 2. Testing mondayWork
+  console
+  
